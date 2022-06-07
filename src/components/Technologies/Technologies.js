931f7b21a37b92aa201960d1169888a37be2abd2
Joseph E <1jojorun@gmail.com>
2022-06-07 15:10:49 -0700

    Updated Project & Footer Information

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -9,7 +9,7 @@ const Technologies = () =>  (
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
       I've worked with a range of technologies in the web development world
-      From Back-end To Design
+      From Front-End To Back-End
     </SectionText>
     <List>
       <ListItem>
